f19e3f636290837a29307ec3538066e9af51d215
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-06-28 00:35:21 +0300

    axios and addContacts

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,13 +1,29 @@
 import { createAction } from '@reduxjs/toolkit';
-import { v4 as uid } from 'uuid';
+import axios from 'axios';
 
-const addContact = createAction('contacts/addItem', ({ name, number }) => ({
-  payload: {
-    id: uid(),
-    name,
-    number,
-  },
-}));
+axios.defaults.baseURL = 'http://localhost:4040';
+
+const addContact =
+  ({ name, number }) =>
+  dispatch => {
+    const contact = { name, number };
+    dispatch({ type: 'contacts/addItemRequest' });
+    axios
+      .post('/contacts', contact)
+      .then(({ data }) =>
+        dispatch({ type: 'contacts/addItemSuccess', payload: data }),
+      )
+      .catch(error =>
+        dispatch({ type: 'contacts/addItemError', payload: error }),
+      );
+  };
+// const addContact = createAction('contacts/addItem', ({ name, number }) => ({
+//   payload: {
+//     id: uid(),
+//     name,
+//     number,
+//   },
+// }));
 
 const deleteContact = createAction('contacts/deleteItem');
 
--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -3,7 +3,7 @@ import { createReducer } from '@reduxjs/toolkit';
 import actions from './contacts-actions';
 
 const itemsReducer = createReducer([], {
-  [actions.addContact]: (state, { payload }) => [...state, payload],
+  'contacts/addItemSuccess': (state, { payload }) => [...state, payload],
   [actions.deleteContact]: (state, { payload }) =>
     state.filter(({ id }) => id !== payload),
 });
